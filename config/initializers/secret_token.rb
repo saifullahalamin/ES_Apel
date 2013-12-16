98316d1e0864bbb91f0fdc5f218ba890e08035ca
Saifullah al amin <saifullah@saifullah-softwareseni.(none)>
2013-12-16 17:26:36 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ESApel::Application.config.secret_key_base = '9d70d8638b35d96e93c2474c06f76764b33b77c5485313d6a02f9c75411aa53f53db565523247d77fff30d5af7ba2ccf13cc8daa843e309ed9e5fb92ba430beb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ESApel::Application.config.session_store :cookie_store, key: '_ES_Apel_session'
